7ab5b302cae8395545ab0b3d3d1f2f4d7dc049ce
frivolta <rivoltafilippo@gmail.com>
2020-10-13 23:57:19 +0200

    Adding create product

--- yumroad/blueprints/products.py
+++ yumroad/blueprints/products.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from yumroad.models import Product
+from yumroad.extensions import db
+from yumroad.forms import ProductForm
 
 products = Blueprint('products', __name__)
 
@@ -14,6 +16,18 @@ def index():
 def details(product_id):
     product = Product.query.get_or_404(product_id)
     return render_template('products/details.html', product=product)
+
+
+@products.route('/create', methods=['GET', 'POST'])
+def create():
+    form = ProductForm()
+    if form.validate_on_submit():
+        product = Product(name=form.name.data,
+                          description=form.description.data)
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('.details', product_id=product.id))
+    return render_template('products/create.html', form=form)
 
 
 @products.errorhandler(404)
--- yumroad/config.py
+++ yumroad/config.py
@@ -7,12 +7,14 @@ class BaseConfig:
 
 class DevConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+    WTF_CSRF_ENABLED = False
     SQLALCHEMY_ECHO = True
 
 
 class TestConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     TESTING = True
+    WTF_CSRF_ENABLED = False
 
 
 class ProdConfig(BaseConfig):
